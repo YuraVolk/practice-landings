02808e92c2369b3ae5ca298adb892c6117ac8dfd
YuraVolk <yura.volkovskiy.2006@gmail.com>
2023-08-17 10:57:43 +0300

    float to bottom without shape outside for box-22

--- js/iframeResize.js
+++ js/iframeResize.js
@@ -1,5 +1,9 @@
 window.resizeTreesSectionIframe = (object) => {
-    setInterval(() => {
+    setTimeout(() => {
         object.style.height = object.contentWindow.document.documentElement.offsetHeight + "px";
     }, 500);
 };
+
+window.addEventListener("resize", () => {
+    window.resizeTreesSectionIframe(document.querySelector(".trees-iframe"));
+});
